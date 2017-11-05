e705f6865af1d7a081b842320775fbd2ca5d3db4
Tristan <tkidder132@gmail.com>
2017-11-05 04:48:33 -0600

    Added music player

---
+++ Assets/_Scripts/MusicPlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MusicPlayerController : MonoBehaviour
+{
+
+    static MusicPlayerController instance = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if(instance)
+        {
+            Destroy(gameObject);
+            Debug.Log("Duplicate Music Player Destroyed");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
