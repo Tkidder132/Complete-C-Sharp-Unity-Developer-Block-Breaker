922021c7bd281815be7385fba4c2613db93637c6
Tristan <tkidder132@gmail.com>
2017-11-08 23:57:08 -0600

    Initial physics, Sprite updates, and credits included in readme

---
+++ Assets/_Scripts/LoseColliderController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LoseColliderController : MonoBehaviour
+{
+    public LevelManagerController levelManager;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Trigger");
+        levelManager.LoadLevel("Lose");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Complete-C-Unity-Developer-Block-Breaker
+
+Credit to Kenney Game Assets for the sprites:
+https://kenney.nl/assets/puzzle-pack
